695a5f9cd195648c0af6e76ccb6c4120af3160b5
Elnur <mikailov.03@list.ru>
2025-11-17 18:44:15 +0300

    Добавление аргументов filepath и параметр format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,8 +9,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0')
-  .action(() => {
-    // Пока пустое действие
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    const options = program.opts();
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
   });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default function genDiff(filepath1, filepath2) {
+export default function genDiff(filepath1, filepath2, format = 'stylish') {
   // Пока возвращаем заглушку
-  return `Comparing ${filepath1} and ${filepath2}`;
+  return `Comparing ${filepath1} and ${filepath2} with format: ${format}`;
 }
