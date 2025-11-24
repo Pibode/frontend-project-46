cd2ad972fad05ed5cdb550bc7735ce0e5916fd93
Elnur <mikailov.03@list.ru>
2025-11-24 21:56:36 +0300

    Исправление конфигурации5

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,9 @@ export default [
       globals: {
         console: 'readonly',
         process: 'readonly',
+        expect: 'readonly',
+        test: 'readonly',
+        describe: 'readonly',
       },
     },
     plugins: {
@@ -22,7 +25,7 @@ export default [
       'linebreak-style': ['error', 'unix'],
       'quotes': ['error', 'single'],
       'semi': ['error', 'never'],
-      '@stylistic/arrow-parens': ['error', 'as-needed'],
+      '@stylistic/arrow-parens': ['error', 'always'],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
       'no-trailing-spaces': 'error',
     },
