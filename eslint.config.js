edb817e507d76e947f0bf56102ab920831bfe7b7
Elnur <mikailov.03@list.ru>
2025-11-24 20:47:51 +0300

    Исправление конфигурации

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly'
+      }
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'arrow-parens': ['error', 'as-needed'],
+      'no-trailing-spaces': 'error'
+    }
+  }
+]
